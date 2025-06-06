f83489b95352b9e980853ac5b34e1d73791569d9
FelipeAugustoFialho <felipefialho.informatica@gmail.com>
2025-06-05 23:27:16 -0300

    add nav,footer mobile

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector("navMobile");
+const list = document.querySelector('li');
+const menuIcon = document.querySelector('.menu-icon');
+const menuOptions = document.querySelector('.menu-options');
+
+
+function toggleMenu() {
+    menuOptions.classList.toggle('active');
+}
+
+
+menuIcon.addEventListener('click', toggleMenu);
